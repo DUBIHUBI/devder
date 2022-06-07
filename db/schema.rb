3c0600f2dee231f434fa3535851320f95e57fc94
Hubert Yu <hubert12161999@gmail.com>
2022-06-07 13:08:54 +0100

    Finished creating users model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_105700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "gender"
+    t.string "location"
+    t.text "bio"
+    t.string "desired_role"
+    t.boolean "student", default: true
+    t.string "role"
+    t.string "company_name"
+    t.string "company_type"
+    t.integer "company_size"
+    t.string "funding_stage"
+    t.string "hq_location"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end

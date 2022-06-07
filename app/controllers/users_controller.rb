d4bd027de6eac189eb05ee0824cd71e63242461e
Hubert Yu <hubert12161999@gmail.com>
2022-06-07 16:40:24 +0100

    Added navbar and user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def my_profile
+    redirect_to root_path unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, path: 'profiles'
+  resources :users, path: 'profiles' do
+    # collection do
+    #   get :my_profile
+    # end
+  end
+
+  get :my_profile, to: "users#my_profile"
   # Defines the root path route ("/")
   # root "articles#index"
 end

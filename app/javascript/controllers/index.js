1a25e6248830b28eb49258cbaf2b074023c131ff
Hubert Yu <hubert12161999@gmail.com>
2022-06-08 17:58:00 +0100

    Added the seed and layout for the index page.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import FilterController from "./filter_controller.js"
+application.register("filter", FilterController)
+
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)

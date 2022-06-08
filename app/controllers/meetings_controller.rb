710d4d893c8ca38fa695e11ddb5955de96d5284a
Anna Efthymiadou <aneuth@gmail.com>
2022-06-08 12:49:49 +0100

    created meetings new page, added book meeting form

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,8 +1,33 @@
 class MeetingsController < ApplicationController
-  def show
-    @meeting = Meeting.find(params[:id])
+  before_action :find_professional, only: [:new, :create]
+
+  # def show
+  #   @meeting = Meeting.find(params[:id])
+  # end
+
+  def new
+    @meeting = Meeting.new
   end
 
   def create
+    @meeting = Meeting.new(meeting_params)
+    @meeting.student = current_user
+    @meeting.professional = @user
+
+    if @meeting.save
+      redirect_to my_profile_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:date, :time)
+  end
+
+  def find_professional
+    @user = User.find(params[:user_id])
   end
 end

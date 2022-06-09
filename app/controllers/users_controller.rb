f03997bd46c50cf58895d1ce45135a44e2009c90
Hubert Yu <hubert12161999@gmail.com>
2022-06-09 17:12:06 +0100

    Working filter with 3 inputs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,36 @@
 class UsersController < ApplicationController
+  # def index
+  #   if params[:query].present? && params[:query] != ""
+  #     sql_query = "first_name ILIKE :query OR last_name ILIKE :query OR company_name ILIKE :query"
+  #     @users = User.where(sql_query, query: "%#{params[:query]}%")
+  #   else
+  #     @users = User.all
+  #   end
+  # end
+
   def index
-    if params[:query].present?
-      sql_query = "first_name ILIKE :query OR last_name ILIKE :query OR company_name ILIKE :query"
-      @users = User.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @users = User.all
-    end
+    sql_query = "first_name ILIKE :query OR last_name ILIKE :query OR company_name ILIKE :query"
+    # mama = "company_type ILIKE :company_type"
+    @users = User.all
+    @users = User.where(sql_query, query: "%#{params[:query]}%") if params[:query].present? && params[:query] != ""
+    # @users = User.where(mama, company_type: "%#{params[:company_type]}%") if params[:company_type].present? && params[:company_type] != ""
+
+    @aaa = params[:company_type]
+
+    @users = @users.select { |user| user.company_type == params[:company_type] } if params[:company_type].present? && params[:company_type] != ""
+
+    @users = @users.select { |user| user.funding_stage == params[:funding_stage] } if params[:funding_stage].present? && params[:funding_stage] != ""
+
+    @users = @users.select { |user| user.gender == params[:gender] } if params[:gender].present? && params[:gender] != ""
+
+    @aaa = params[:gender]
+
   end
+
+  # def index
+  #   @users = @listings.where("name = ?", params[:name]) if params[:name].present? && params[:name] != ""
+  #   @users = @listings.where("employment_type = ?", params[:employment_type]) if params[:employment_type].present? && params[:employment_type] != ""
+  # end
 
   def show
     @user = User.find(params[:id])
--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -2,20 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="filter"
 export default class extends Controller {
+  static targets = ["search"];
+
   connect() {
     console.log("Stimulus controller connected!")
   }
+
+  search() {
+    const url = `${"http://localhost:3000/profiles"}?${this.params}`;
+
+    Turbolinks.clearCache();
+    Turbolinks.visit(url);
+  }
+
+  get params() {
+    return this.searchTargets.map((t) => `${t.name}=${t.value}`).join("&");
+  }
 }
-//   static targets = ["filter"];
-
-//   filter() {
-//     const url = `${window.location.pathname}?${this.params}`;
-
-//     Turbolinks.clearCache();
-//     Turbolinks.visit(url);
-//   }
-
-//   get params() {
-//     return this.filterTargets.map((t) => `${t.name}=${t.value}`).join("&");
-//   }
-// }

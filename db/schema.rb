0556f15a76e46b0aa1913ceea4004bdd3e582574
Carly Luo <carlythelaw@gmail.com>
2022-06-09 11:31:25 +0100

    updated users column location to address for mapping function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_101407) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_102900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_101407) do
     t.string "last_name"
     t.integer "age"
     t.string "gender"
-    t.string "location"
+    t.string "address"
     t.text "bio"
     t.string "desired_role"
     t.boolean "student", default: true

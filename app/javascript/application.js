26f3a0805aad6add66c6d58f04ad28734adc2a38
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-06-14 11:19:37 +0100

    Disabled ability for student to review a professional multiple times

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+import "star-rating.js"
+// import "star-rating.css"
+
+document.addEventListener("turbolinks:load", function(event) {
+  let stars = new StarRating('.star-rating');
+});

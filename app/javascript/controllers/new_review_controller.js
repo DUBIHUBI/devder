3bd095d7a8ca47aef848039ff07cbc7d53b9c877
Johan van Meeuwen <vanmeeuwen.j@gmail.com>
2022-06-15 07:08:41 +0100

    Made ability to cross-off/cancel review form pop-up

--- app/javascript/controllers/new_review_controller.js
+++ app/javascript/controllers/new_review_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-review"
 export default class extends Controller {
-  static targets = ["button", "form", "avatar", "submit"]
+  static targets = ["button", "form", "avatar", "submit", "cross"]
 
   revealContent(event) {
     event.preventDefault()
     this.avatarTarget.classList.add("d-none")
     this.buttonTarget.classList.add("d-none")
     this.formTarget.classList.remove("d-none")
+  }
+
+  closeContent(event) {
+    this.avatarTarget.classList.remove("d-none")
+    this.buttonTarget.classList.remove("d-none")
+    this.formTarget.classList.add("d-none")
   }
 
   // create() {
